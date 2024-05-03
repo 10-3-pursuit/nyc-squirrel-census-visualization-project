ba71582cab39ce023264bbd44bcef1d607e191a1
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-05-03 13:38:45 -0400

    Updated with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   return (
     <>
       <h1>Squirrel Census Visualization Starter</h1>
+      <h1 className="text-3xl font-bold underline bg-blue-400">
+      Hello world!
+      </h1>
       <Routes>
         <Route />
       </Routes>
