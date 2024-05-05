3edc1eaa4ede6372347bc8788d721a06ce8ebe88
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-05-05 00:02:43 -0400

    Updated SquirrelIndexHelper with a region to location converter function to reverse prevous functions outputs

--- src/Helpers/SquirrelIndexHelper.js
+++ src/Helpers/SquirrelIndexHelper.js
@@ -42,3 +42,34 @@ export function getRegion(location) {
     return region;
 }
 
+export function reverseRegion(region) {
+    // Define an object mapping each region to its corresponding range of hectare-letter combinations
+    const regionRanges = {
+        'EastSouth': { start: 1, end: 14, letters: ['A', 'B', 'C'] },
+        'EastCenter': { start: 15, end: 28, letters: ['A', 'B', 'C'] },
+        'EastNorth': { start: 29, end: 42, letters: ['A', 'B', 'C'] },
+        'CenterSouth': { start: 1, end: 14, letters: ['D', 'E', 'F'] },
+        'Center': { start: 15, end: 28, letters: [ 'D', 'E', 'F'] },
+        'CenterNorth': { start: 29, end: 42, letters: ['D', 'E', 'F'] },
+        'WestSouth': { start: 1, end: 14, letters: ['G', 'H', 'I'] },
+        'WestCenter': { start: 15, end: 28, letters: ['G', 'H', 'I'] },
+        'WestNorth': { start: 29, end: 42, letters: ['G', 'H', 'I'] },
+    };
+
+    // Check if the region exists in the regionRanges object
+    if (!(region in regionRanges)) {
+        return [];
+    }
+
+    const { start, end, letters } = regionRanges[region];
+
+    // Iterate over the range for the given region and construct the array of hectare-letter pairs
+    const result = [];
+    for (let number = start; number <= end; number++) {
+        letters.forEach(letter => {
+            result.push(`${number<=9?"0"+number:number}${letter}`);
+        });
+    }
+    console.log(result)
+    return result;
+}
